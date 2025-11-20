fb95ea547db55f26e1f782c04c00a26c6da6d2c3
predator <smartymin47@gmail.com>
2025-11-20 14:20:41 +0500

    Fix syntax in main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,11 +1,10 @@
-// frontend/src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './App.css' // or './App.css' depending on what you have
+import App from './app.jsx'
+import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
